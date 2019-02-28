0a309375655e33a0f490c9e1f8f46a129495ae50
vincent VB <42698353+vincentbudianto@users.noreply.github.com>
2019-02-28 23:12:47 +0700

    versi animal yang satu class satu file

---
+++ Animal/IProducing.hpp
@@ -0,0 +1,27 @@
+/* NIM/Nama  : 13517137/Vincent Budianto
+ * Nama file : IProducing.hpp
+ * Topik     : Tugas Besar 1 IF2210 - Pemograman Berorientasi Objek
+ * Tanggal   : 20 Maret 2019
+ * Deskripsi : Header IProducing.cpp */
+
+#ifndef IPRODUCING_HPP
+#define IPRODUCING_HPP
+
+#include "FarmAnimal.hpp"
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class IProducing: public FarmAnimal
+{
+	public:
+		virtual void interactProduct() = 0;
+};
+
+class KProducing: public FarmAnimal
+{
+	public:
+		virtual void killProduct() = 0;
+};
+
+#endif
